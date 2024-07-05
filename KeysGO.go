5b83e07e8da8d0ee38c15904635ca55e65d8a3f9
Dog-Gabriel07 <senpaizxjoga@gmail.com>
2024-07-04 21:47:58 -0300

    Update KeysGO.go
    0.02V Novas Funções e Bugs Corrigidos

--- KeysGO.go
+++ KeysGO.go
@@ -23,7 +23,7 @@ func clearScreen() {
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	default:
-		fmt.Println("Cannot clear terminal screen for this OS")
+		fmt.Println("Não foi possível limpar a tela para este sistema operacional")
 	}
 }
 
@@ -38,98 +38,63 @@ func setTitle(title string) {
 	case "darwin":
 		fmt.Printf("\033]0;%s\007", title)
 	default:
-		fmt.Println("Cannot set terminal title for this OS")
+		fmt.Println("Não foi possível definir o título do terminal para este sistema operacional")
 	}
 }
 
-func parseNumber(input string) (*big.Int, error) {
-	input = strings.ToLower(input)
-	input = strings.ReplaceAll(input, ",", ".")
-
-	re := regexp.MustCompile(`(\d*\.?\d*)([kmgt]?)`)
-	matches := re.FindStringSubmatch(input)
-	if len(matches) < 3 {
-		return nil, fmt.Errorf("entrada inválida")
-	}
-
-	baseStr := matches[1]
-	suffix := matches[2]
-
-	base, err := strconv.ParseFloat(baseStr, 64)
-	if err != nil {
-		return nil, fmt.Errorf("entrada inválida")
-	}
-
-	switch suffix {
-	case "k":
-		base *= 1e3
-	case "m":
-		base *= 1e6
-	case "b":
-		base *= 1e9
-	case "t":
-		base *= 1e12
-	}
-
-	return new(big.Int).SetInt64(int64(base)), nil
-}
-
-func calculateTimeToCrack(bits int, keysPerSecond *big.Int) string {
+func calculateTimeToCrack(bits int, keysPerSecond *big.Int) float64 {
 	totalCombinations := new(big.Int).Exp(big.NewInt(2), big.NewInt(int64(bits)), nil)
 	timeInSeconds := new(big.Int).Div(totalCombinations, keysPerSecond)
-	secondsInAYear := big.NewInt(60 * 60 * 24 * 365)
+	secondsInAYear := big.NewInt(31536000) // segundos em um ano padrão (60 * 60 * 24 * 365)
 	timeInYears := new(big.Float).Quo(new(big.Float).SetInt(timeInSeconds), new(big.Float).SetInt(secondsInAYear))
-
-	years := new(big.Float).Quo(timeInYears, big.NewFloat(1))
-	yearsInt, _ := years.Int(nil)
-
-	if yearsInt.Sign() == 0 {
-		// Menos de um ano
-		months := new(big.Float).Mul(years, big.NewFloat(12))
-		monthsInt, _ := months.Int(nil)
-
-		seconds := new(big.Float).SetFloat64(0)
-		if monthsInt.Sign() > 0 {
-			monthsSeconds := new(big.Float).Mul(months, big.NewFloat(365*24*60*60/12))
-			seconds = new(big.Float).Sub(timeInYears, new(big.Float).SetFloat64(float64(monthsInt.Int64())/12)).Mul(monthsSeconds, big.NewFloat(12))
-		} else {
-			seconds = timeInYears
-		}
-
-		return fmt.Sprintf("%d meses %.2f segundos", monthsInt, seconds)
-	} else {
-		// Um ano ou mais
-		return fmt.Sprintf("%d anos", yearsInt)
-	}
+	timeToCrack, _ := timeInYears.Float64()
+	return timeToCrack
 }
 
-func calculateProgress(bits int, keysPerSecond, keysChecked *big.Int) string {
+func calculateProgress(bits int, keysPerSecond, keysChecked *big.Int) float64 {
 	totalCombinations := new(big.Int).Exp(big.NewInt(2), big.NewInt(int64(bits)), nil)
 	keysLeft := new(big.Int).Sub(totalCombinations, keysChecked)
 	timeInSeconds := new(big.Int).Div(keysLeft, keysPerSecond)
-	secondsInAYear := big.NewInt(60 * 60 * 24 * 365)
+	secondsInAYear := big.NewInt(31536000) // segundos em um ano padrão (60 * 60 * 24 * 365)
 	timeInYears := new(big.Float).Quo(new(big.Float).SetInt(timeInSeconds), new(big.Float).SetInt(secondsInAYear))
+	timeToFinish, _ := timeInYears.Float64()
+	return timeToFinish
+}
 
-	years := new(big.Float).Quo(timeInYears, big.NewFloat(1))
-	yearsInt, _ := years.Int(nil)
-
-	if yearsInt.Sign() == 0 {
-		// Menos de um ano
-		months := new(big.Float).Mul(years, big.NewFloat(12))
-		monthsInt, _ := months.Int(nil)
-
-		seconds := new(big.Float).SetFloat64(0)
-		if monthsInt.Sign() > 0 {
-			monthsSeconds := new(big.Float).Mul(months, big.NewFloat(365*24*60*60/12))
-			seconds = new(big.Float).Sub(timeInYears, new(big.Float).SetFloat64(float64(monthsInt.Int64())/12)).Mul(monthsSeconds, big.NewFloat(12))
-		} else {
-			seconds = timeInYears
+func parseKeysPerSecond(input string) (*big.Int, error) {
+	input = strings.ToLower(input)
+	if strings.Contains(input, "m") {
+		value, err := strconv.ParseFloat(strings.TrimRight(input, "m"), 64)
+		if err != nil {
+			return nil, err
 		}
-
-		return fmt.Sprintf("%d meses %.2f segundos", monthsInt, seconds)
+		return new(big.Int).Mul(big.NewInt(int64(value)), big.NewInt(1000000)), nil
+	} else if strings.Contains(input, "b") {
+		value, err := strconv.ParseFloat(strings.TrimRight(input, "b"), 64)
+		if err != nil {
+			return nil, err
+		}
+		return new(big.Int).Mul(big.NewInt(int64(value)), big.NewInt(1000000000)), nil
+	} else if strings.Contains(input, "t") {
+		value, err := strconv.ParseFloat(strings.TrimRight(input, "t"), 64)
+		if err != nil {
+			return nil, err
+		}
+		return new(big.Int).Mul(big.NewInt(int64(value)), big.NewInt(1000000000000)), nil
+	} else if strings.Contains(input, ",") {
+		value, err := strconv.ParseFloat(strings.Replace(input, ",", "", -1), 64)
+		if err != nil {
+			return nil, err
+		}
+		return new(big.Int).Mul(big.NewInt(int64(value)), big.NewInt(1000)), nil
+	} else if strings.Contains(input, ".") {
+		value, err := strconv.ParseFloat(strings.Replace(input, ".", "", -1), 64)
+		if err != nil {
+			return nil, err
+		}
+		return new(big.Int).Mul(big.NewInt(int64(value)), big.NewInt(10)), nil
 	} else {
-		// Um ano ou mais
-		return fmt.Sprintf("%d anos", yearsInt)
+		return nil, fmt.Errorf("Formato inválido")
 	}
 }
 
@@ -139,10 +104,7 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	green := "\033[32m"
-	reset := "\033[0m"
-
-	fmt.Println(green + `
+	fmt.Println(`
 ▀████▀ ▀███▀                           ▄▄█▀▀▀█▄█  ▄▄█▀▀██▄  
   ██   ▄█▀                           ▄██▀     ▀█▄██▀    ▀██▄
   ██ ▄█▀      ▄▄█▀██▀██▀   ▀██▀▄██▀████▀       ▀██▀      ▀██
@@ -151,28 +113,23 @@ func main() {
   ██   ▀██▄  ██▄    ▄   ███    █▄   ████▄     ██▀██▄    ▄██▀
 ▄████▄   ███▄ ▀█████▀   ▄█     ██████▀ ▀▀███████  ▀▀████▀▀  
                       ▄█                                    
-                    ██▀                                     
-` + reset)
-
-	fmt.Println(green + "Creditos: Dog_Gabriel" + reset)
+                    ██▀                                    `)
 
 	for {
+		fmt.Println("Escolha uma opção:")
 		fmt.Println("1. Calcular tempo necessário para quebrar uma chave")
 		fmt.Println("2. Calcular progresso com base no número de chaves já verificadas")
-		fmt.Println("Pressione Ctrl + C para sair do programa")
-
-		fmt.Print("Escolha uma opção: ")
+		fmt.Println("3. Estimativas de custo computacional (em desenvolvimento)")
+		fmt.Println("4. Eficiência energética (em desenvolvimento)")
+		fmt.Println("5. Ajuda")
+		fmt.Println("6. Sair")
+		fmt.Print("Opção: ")
 
 		choiceStr, _ := reader.ReadString('\n')
 		choiceStr = strings.TrimSpace(choiceStr)
-		choice, err := strconv.Atoi(choiceStr)
-		if err != nil || (choice != 1 && choice != 2) {
-			fmt.Println("Opção inválida")
-			continue
-		}
 
-		switch choice {
-		case 1:
+		switch choiceStr {
+		case "1":
 			fmt.Print("Digite o número de bits da chave: ")
 			bitsStr, _ := reader.ReadString('\n')
 			bitsStr = strings.TrimSpace(bitsStr)
@@ -182,19 +139,20 @@ func main() {
 				continue
 			}
 
-			fmt.Print("Digite o número de chaves verificadas por segundo: ")
+			fmt.Print("Digite a taxa de chaves verificadas por segundo (ex. 15M, 2.5k, 45,9k): ")
 			keysPerSecondStr, _ := reader.ReadString('\n')
 			keysPerSecondStr = strings.TrimSpace(keysPerSecondStr)
-			keysPerSecond, err := parseNumber(keysPerSecondStr)
+
+			keysPerSecond, err := parseKeysPerSecond(keysPerSecondStr)
 			if err != nil {
-				fmt.Println("Entrada inválida para o número de chaves por segundo")
+				fmt.Println("Entrada inválida para a taxa de chaves verificadas por segundo")
 				continue
 			}
 
 			timeToCrack := calculateTimeToCrack(bits, keysPerSecond)
-			fmt.Printf("Tempo estimado para testar todas as combinações de uma carteira de %d bits a %.2f chaves por segundo: %s\n", bits, float64(keysPerSecond.Int64())/1e6, timeToCrack)
+			fmt.Printf("Levaria aproximadamente %.2f anos para testar todas as combinações de uma carteira de %d bits a uma taxa de %s chaves por segundo.\n", timeToCrack)
 
-		case 2:
+		case "2":
 			fmt.Print("Digite o número de bits da chave: ")
 			bitsStr, _ := reader.ReadString('\n')
 			bitsStr = strings.TrimSpace(bitsStr)
@@ -204,26 +162,67 @@ func main() {
 				continue
 			}
 
-			fmt.Print("Digite o número de chaves verificadas por segundo: ")
+			fmt.Print("Digite a taxa de chaves verificadas por segundo (ex. 15M, 2.5k, 45,9k): ")
 			keysPerSecondStr, _ := reader.ReadString('\n')
 			keysPerSecondStr = strings.TrimSpace(keysPerSecondStr)
-			keysPerSecond, err := parseNumber(keysPerSecondStr)
+
+			keysPerSecond, err := parseKeysPerSecond(keysPerSecondStr)
 			if err != nil {
-				fmt.Println("Entrada inválida para o número de chaves por segundo")
+				fmt.Println("Entrada inválida para a taxa de chaves verificadas por segundo")
 				continue
 			}
 
 			fmt.Print("Digite o número de chaves já verificadas: ")
 			keysCheckedStr, _ := reader.ReadString('\n')
 			keysCheckedStr = strings.TrimSpace(keysCheckedStr)
-			keysChecked, err := parseNumber(keysCheckedStr)
-			if err != nil {
+			keysChecked, ok := new(big.Int).SetString(keysCheckedStr, 10)
+			if !ok {
 				fmt.Println("Entrada inválida para o número de chaves verificadas")
 				continue
 			}
 
-			progress := calculateProgress(bits, keysPerSecond, keysChecked)
-			fmt.Printf("Tempo estimado para testar as combinações restantes de uma carteira de %d bits a %.2f chaves por segundo: %s\n", bits, float64(keysPerSecond.Int64())/1e6, progress)
+			timeToFinish := calculateProgress(bits, keysPerSecond, keysChecked)
+			fmt.Printf("Levaria aproximadamente %.2f anos para testar as combinações restantes de uma carteira de %d bits a uma taxa de %s chaves por segundo.\n", timeToFinish)
+
+		case "3":
+			fmt.Println("Opção em desenvolvimento: Estimativas de custo computacional")
+			// Implementar funcionalidade de estimativas de custo computacional aqui
+
+		case "4":
+			fmt.Println("Opção em desenvolvimento: Eficiência energética")
+			// Implementar funcionalidade de eficiência energética aqui
+
+		case "5", "ajuda":
+			clearScreen()
+			setTitle("KeysGo - Ajuda")
+
+			fmt.Println("Bem-vindo ao KeysGo!")
+			fmt.Println("Este programa oferece várias funcionalidades relacionadas à quebra de chaves criptográficas e cálculos relacionados.")
+			fmt.Println("Aqui estão as opções disponíveis:")
+			fmt.Println("1. Calcular tempo necessário para quebrar uma chave")
+			fmt.Println("   - Esta opção calcula quanto tempo levaria para testar todas as combinações possíveis de uma chave criptográfica.")
+			fmt.Println("2. Calcular progresso com base no número de chaves já verificadas")
+			fmt.Println("   - Esta opção estima quanto tempo falta para testar as combinações restantes de uma chave criptográfica, com base em chaves já verificadas.")
+			fmt.Println("3. Estimativas de custo computacional (em desenvolvimento)")
+			fmt.Println("   - Opção futura para calcular o custo computacional estimado para quebrar uma chave criptográfica.")
+			fmt.Println("4. Eficiência energética (em desenvolvimento)")
+			fmt.Println("   - Opção futura para calcular a eficiência energética envolvida na quebra de chaves criptográficas.")
+			fmt.Println("5. Ajuda")
+			fmt.Println("   - Exibe esta mensagem de ajuda.")
+			fmt.Println("6. Sair")
+			fmt.Println("   - Encerra o programa.")
+
+			fmt.Print("\nPressione Enter para voltar ao menu principal...")
+			reader.ReadString('\n')
+			clearScreen()
+			setTitle("KeysGo")
+
+		case "6", "sair":
+			fmt.Println("Encerrando o programa...")
+			return
+
+		default:
+			fmt.Println("Opção inválida. Escolha uma das opções disponíveis.")
 		}
 	}
 }
